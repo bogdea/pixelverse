246f26850957af6963fdd6d7563f7c15a2b12e66
Madalin Bogdea <madalinbogdea@icloud.com>
2024-03-16 15:02:58 +0200

    add stats section

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,32 @@
+// hamburger menu
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const mobileNavbar = document.querySelector(".mobile-navbar");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   mobileNavbar.classList.toggle("top-[1000px]");
+});
+
+// counter
+const counterUp = window.counterUp.default;
+
+const callback = (entries) => {
+  entries.forEach((entry) => {
+    const el = entry.target;
+    if (entry.isIntersecting && !el.classList.contains("is-visible")) {
+      counterUp(el, {
+        duration: 2000,
+        delay: 16,
+      });
+      el.classList.add("is-visible");
+    }
+  });
+};
+
+const IO = new IntersectionObserver(callback, { threshold: 1 });
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  IO.observe(counter);
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+      },
+    },
   },
   plugins: [],
 };
